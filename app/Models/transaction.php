e2845fcae013d2e68d8c9f2c3acdccdf7cdccaa3
Alex Muiruri <content@alexmuiruri.com>
2026-02-24 15:17:34 +0300

    feat:created user, wallet, and transaction relationships and fillables

--- app/Models/transaction.php
+++ app/Models/transaction.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class transaction extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'wallet_id',
+        'transaction_type',
+        'transaction_amount',
+        'transaction_description',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\user');
+    }
+
+    public function wallet()
+    {
+        return $this->belongsTo('App\Models\wallet');
+    }
 }
--- app/Models/wallet.php
+++ app/Models/wallet.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class wallet extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'wallet_name',
+        'wallet_address',
+        'wallet_balance',
+        'wallet_description',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\user');
+    }
 }
