0531881779a6e1aaa902cb1ff28f8d316e60bd91
Alex Muiruri <content@alexmuiruri.com>
2026-02-24 15:33:58 +0300

    feat:Updated home text

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,9 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'api'], function () {
     // API Routes
     Route::get('/', function () {
-        return 'API is working!';
+        return response()->json([
+            'message' => 'Welcome to the Money Tracker API!',
+        ]);
     });
 
     // User Routes
