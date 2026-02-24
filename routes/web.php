53833a378ba02a36083b3d71d62a99fdbbf91956
Alex Muiruri <content@alexmuiruri.com>
2026-02-24 15:11:41 +0300

    feat:created models and controllers and configured the api routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WalletController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'api'], function () {
+    // API Routes
+    Route::get('/', function () {
+        return 'API is working!';
+    });
+
+    // User Routes
+    Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
+
+    // Wallet Routes
+    Route::get('/wallets', [WalletController::class, 'index']);
+    Route::post('/wallets', [WalletController::class, 'store']);
+    Route::get('/wallets/{wallet}', [WalletController::class, 'show']);
+
+    // Transaction Routes
+    Route::get('/transactions', [TransactionController::class, 'index']);
+    Route::post('/transactions', [TransactionController::class, 'store']);
+    Route::get('/transactions/{transaction}', [TransactionController::class, 'show']);
 });
